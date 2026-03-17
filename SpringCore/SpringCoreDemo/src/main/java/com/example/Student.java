f14cbf8431764e359a891b2ab451473eb20901d5
Jeevan Sagar <vtu27264@veltech.edu.in>
2026-03-17 11:50:26 +0530

    Add files via upload

---
+++ SpringCore/SpringCoreDemo/src/main/java/com/example/Student.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Student {
+	private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("Student Name: " + name);
+    }
+}
