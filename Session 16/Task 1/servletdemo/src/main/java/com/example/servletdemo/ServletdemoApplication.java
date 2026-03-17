9dbb3b5fbdca7b1eb212fdc570c73fb70f1a8d40
Jeevan Sagar <vtu27264@veltech.edu.in>
2026-03-17 11:44:30 +0530

    Add files via upload

---
+++ Session 16/Task 1/servletdemo/src/main/java/com/example/servletdemo/ServletdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.servletdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletdemoApplication.class, args);
+	}
+
+}
---
+++ Session 16/Task 1/servletdemo/src/main/java/com/example/servletdemo/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.servletdemo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Servlet Application Running";
+    }
+}
